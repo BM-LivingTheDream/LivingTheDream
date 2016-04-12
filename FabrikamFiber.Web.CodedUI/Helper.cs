30ba37e6fbb507800a5fd512f6e7333e98d85236
Richard Fennell <richard@blackmarble.co.uk>
2016-04-12 11:16:47 +0100

    Moved the codedui test url to a file

--- FabrikamFiber.Web.CodedUI/Helper.cs
+++ FabrikamFiber.Web.CodedUI/Helper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,12 @@ namespace FabrikamFiber.Web.CodedUI
         {
             get
             {
-                return new Uri("http://localhost:16535/");
+                var baseuri = ConfigurationManager.AppSettings["weburi"];
+                if (baseuri == null)
+                {
+                    throw new NullReferenceException("No baseuri definded");
+                }
+                return new Uri(baseuri);
             }
         }
     }
