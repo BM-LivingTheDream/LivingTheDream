3eef35718251d9d60ed969722e918a2375e6b5d9
Richard Fennell <richard@blackmarble.co.uk>
2016-04-12 10:59:29 +0100

    Moved the tests to a folder Added API tests

---
+++ FabrikamFiber.Web.IntegrationTests/ApiTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+using System.Dynamic;
+
+namespace FabrikamFiber.Web.IntegrationTests
+{
+    [TestClass]
+    public class ApiTests
+    {
+        [TestMethod]
+        public void GetCustomers()
+        {
+            // arrange
+            var wc = Helper.WebClient();
+
+            // act
+            var data = wc.DownloadString(Helper.WebUrl("/Api/Customers"));
+
+            // assert
+            // do a bit a parsing to make asserts easier
+            var actual = Helper.ParseJson(data, new[] { new { id = 0, FirstName = "", LastName = "" } });
+               
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(11, actual.Count);
+        }
+
+        [TestMethod]
+        public void GetEmployees()
+        {
+            // arrange
+            var wc = Helper.WebClient();
+
+            // act
+            var data = wc.DownloadString(Helper.WebUrl("/Api/Employees"));
+
+            // assert
+            // do a bit a parsing to make asserts easier
+            var actual = Helper.ParseJson(data, new[] { new { id = 0, FirstName = "", LastName = "" } });
+
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(4, actual.Count);
+        }
+
+        [TestMethod]
+        public void GetServiceTickets()
+        {
+            // arrange
+            var wc = Helper.WebClient();
+
+            // act
+            var data = wc.DownloadString(Helper.WebUrl("/Api/ServiceTickets"));
+
+            // assert
+            // do a bit a parsing to make asserts easier
+            var actual = Helper.ParseJson(data, new[] { new { id = 0, Status = "", Title = "" } });
+
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(9, actual.Count);
+        }
+    }
+
+
+}
---
+++ FabrikamFiber.Web.IntegrationTests/Helper.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabrikamFiber.Web.IntegrationTests
+{
+    public class Helper
+    {
+        public static WebClient WebClient()
+        {
+            var wc = new WebClient();
+            wc.Headers["Content-Type"] = "application/json";
+            wc.UseDefaultCredentials = true;
+            return wc;
+        }
+
+        public static dynamic ParseJson(string data, object dummyObject)
+        {
+            // do a bit a parsing to make asserts easier
+            return JsonConvert.DeserializeAnonymousType(data, dummyObject);
+        }
+
+        public static Uri WebUrl(string path)
+        {
+            var baseuri = ConfigurationManager.AppSettings["weburi"];
+            if (baseuri == null)
+            {
+                throw new NullReferenceException("No baseuri definded");
+            }
+            return new Uri(string.Format("{0}{1}", baseuri, path));
+        }
+    }
+}
