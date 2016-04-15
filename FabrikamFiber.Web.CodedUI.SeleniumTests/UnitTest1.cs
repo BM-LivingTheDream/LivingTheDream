5db3acf5d3003c93a2c9a82265ab237f7974bf7f
Richard Fennell <richard@blackmarble.co.uk>
2016-04-15 16:51:38 +0100

    Added selenium tests

---
+++ FabrikamFiber.Web.CodedUI.SeleniumTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FabrikamFiber.Web.CodedUI.SeleniumTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ FabrikamFiber.Web.SeleniumTests/CustomerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.PhantomJS;
+
+
+namespace FabrikamFiber.Web.SeleniumTests
+{
+    [TestClass]
+    public class CustomerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var driver = new PhantomJSDriver())
+            {
+                var page = new SeleniumPage(driver);
+                page.GoTo(Helper.WebUrl.ToString());
+                page.FindLink("Customers").Click();
+
+                int oldRowCount = page.TableRowCount("dataTable");
+                Console.Write(oldRowCount);
+                page.FindLink("Create New").Click();
+                page.FindTextBox("FirstName").SendKeys("Fred");
+                page.FindTextBox("LastName").SendKeys("Bloggs");
+                page.FindTextBox("Address.Street").SendKeys("1 The Road");
+                page.FindTextBox("Address.City").SendKeys("Townsville");
+                page.FindTextBox("Address.State").SendKeys("Countyshire");
+                page.FindTextBox("Address.Zip").SendKeys("12345");
+                page.FindButton("Create").Click();
+
+                var newRowCount = page.TableRowCount("dataTable");
+
+                Assert.IsTrue(newRowCount - oldRowCount == 1);
+
+                driver.Quit();
+            }
+
+        }
+    }
+}
