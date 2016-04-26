3e6c9e9f70f5cbb7910d31a37fdafa204645a27c
Richard Fennell <richard@blackmarble.co.uk>
2016-04-26 09:54:44 +0100

    Add test data control flag

---
+++ FabrikamFiber.Web/Helpers/FeatureFlag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FabrikamFiber.Web.Helpers
+{
+    public class FeatureFlag
+    {
+        public const string GENERATETESTDATA = "GENERATETESTDATA";
+
+        public static bool IsFlagSet(string flag)
+        {
+            var retValue = false;
+            if (string.IsNullOrEmpty(flag) == false)
+            {
+                try
+                {
+                    bool.TryParse(System.Configuration.ConfigurationManager.AppSettings[flag], out retValue);
+
+                }
+                catch (System.Configuration.ConfigurationErrorsException)
+                { }
+            }
+            return retValue;
+        }
+    }
+}
